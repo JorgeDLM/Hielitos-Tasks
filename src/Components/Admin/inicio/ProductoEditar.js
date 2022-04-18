40644ad1386e94013e3a23cbc9e938782b0317ab
Jorge <palombaco@live.com>
2022-04-18 15:30:47 -0500

    intento ios

--- src/Components/Admin/inicio/ProductoEditar.js
+++ src/Components/Admin/inicio/ProductoEditar.js
@@ -17,8 +17,8 @@ function ProductoEditar(props) {
             <Link 
                 to={`/admin/editar/${props.p.id}`} 
                 target="_blank" 
-                className="linkSF widthCardProveedor fondoRojo">
-                <Card className={`pmediano claseCard widthCardProveedor centradoRelativo mouseSelectClick ${props.p.propietario === "Ana y Jorge" && "fondoAzulClaro"} ${props.p.propietario === "Ana" && "fondoRojoClaro"}`}>
+                className="linkSF widthCardProveedor centradoRelativo fondoRojo">
+                <Card className={`pmediano claseCard widthCardProveedor mouseSelectClick ${props.p.propietario === "Ana y Jorge" && "fondoAzulClaro"} ${props.p.propietario === "Ana" && "fondoRojoClaro"}`}>
                     <div className="contenedor">
                         <img src={props.p.imagen_thumbnail} className="claseImagenCatalogo" alt="error" />
                         <div className="gris centro parmediano">{nombre}</div>
